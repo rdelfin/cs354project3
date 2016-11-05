fed7e3fef6c5908a0d20efcc3b99505f33249916
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2016-11-05 14:27:44 -0500

    Partial skeleton commit

--- include/skeleton.h
+++ include/skeleton.h
@@ -7,6 +7,39 @@
 #define GLSL_SKELETON_HPP
 
 #include <glm/glm.hpp>
+#include <vector>
+
+
+/**
+ * Class that represents the individual nodes in the skeleton DAG
+ */
+class Joint {
+public:
+    /**
+     * Creates the joint with coordinates relative to the parent. If there is no parent, the coordinates are in
+     * world coordinates.
+     * @param origin The position offset relative to the parent
+     * @param up The up vector for the joint, where (0, 1, 0) is the up vector for the parent
+     * @param right The right vector for the joint, defined the same way as the up vector
+     * @param parent The parent node of the joint. Null if this is the root node
+     */
+    Joint(glm::vec3 origin, glm::vec3 up, glm::vec3 right, Joint* parent);
+
+    void addChild(Joint*);
+
+    std::vector<Joint*> pathTo(Joint* joint);
+private:
+    glm::mat4 rotation;
+    glm::mat4 translation;
+
+    glm::vec4 origin;
+    glm::vec4 origin_up;
+    glm::vec4 origin_right;
+
+    Joint* parent;
+    std::vector<Joint*> children;
+};
+
 
 /**
  * Class that represents the skeleton for a character. Contains a full list of all nodes
@@ -14,22 +47,14 @@
 class Skeleton {
 public:
     Skeleton();
-    Skeleton(std::vector<glm::vec3> joints);
+    Skeleton(Joint* root);
+
+    glm::vec4 transform(glm::vec4 point, Joint* joint);
 private:
     Joint* root;
     std::vector<Joint> joints;
-};
 
-/**
- * Class that represents the individual nodes in the skeleton DAG
- */
-class Joint {
-public:
-    Joint(glm::mat4 rotation, glm::mat4 translation, Joint* parent);
-private:
-    glm::mat4 rotation;
-    glm::mat4 translation;
-    Joint* parent;
+    std::vector<Joint*> pathTo(Joint* joint);
 };
 
 
--- src/skeleton.cpp
+++ src/skeleton.cpp
@@ -1,1 +1,31 @@
 #include "skeleton.h"
+
+
+Joint::Joint(glm::vec3 origin, glm::vec3 up, glm::vec3 right, Joint* parent)
+    : origin(glm::vec4(origin, 1)), origin_up(glm::vec4(up, 0)), origin_right(glm::vec4(right, 0)), parent(parent)
+{
+
+}
+
+std::vector<Joint*> Joint::pathTo(Joint *joint) {
+
+}
+
+void Joint::addChild(Joint* child) {
+    if(child != nullptr)
+        children.push_back(child);
+}
+
+
+Skeleton::Skeleton(Joint* root)
+    : root(root) {
+
+}
+
+glm::vec4 Skeleton::transform(glm::vec4 point, Joint* joint) {
+
+}
+
+std::vector<Joint*> Skeleton::pathTo(Joint* joint) {
+    return root->pathTo(joint);
+}
