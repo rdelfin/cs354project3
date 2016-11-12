27de666fa1a6d19b350e89ccc99b88d1612a435c
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2016-11-11 23:32:29 -0600

    Changed tabs to spaces in bone_geometry.cc

--- src/bone_geometry.cc
+++ src/bone_geometry.cc
@@ -11,16 +11,16 @@
  */
 template <typename T>
 std::ostream& operator<<(std::ostream& os, const std::vector<T>& v) {
-	size_t count = std::min(v.size(), static_cast<size_t>(10));
-	for (size_t i = 0; i < count; ++i) os << i << " " << v[i] << "\n";
-	os << "size = " << v.size() << "\n";
-	return os;
+    size_t count = std::min(v.size(), static_cast<size_t>(10));
+    for (size_t i = 0; i < count; ++i) os << i << " " << v[i] << "\n";
+    os << "size = " << v.size() << "\n";
+    return os;
 }
 
 std::ostream& operator<<(std::ostream& os, const BoundingBox& bounds)
 {
-	os << "min = " << bounds.min << " max = " << bounds.max;
-	return os;
+    os << "min = " << bounds.min << " max = " << bounds.max;
+    return os;
 }
 
 
@@ -38,11 +38,11 @@ Mesh::~Mesh()
 
 void Mesh::loadpmd(const std::string& fn)
 {
-	MMDReader mr;
-	mr.open(fn);
-	mr.getMesh(vertices, faces, vertex_normals, uv_coordinates);
-	computeBounds();
-	mr.getMaterial(materials);
+    MMDReader mr;
+    mr.open(fn);
+    mr.getMesh(vertices, faces, vertex_normals, uv_coordinates);
+    computeBounds();
+    mr.getMaterial(materials);
 
 
     // Fetch all joints and create a skeleton based off of that
@@ -63,19 +63,19 @@ void Mesh::loadpmd(const std::string& fn)
 
 void Mesh::updateAnimation()
 {
-	animated_vertices = vertices;
-	// FIXME: blend the vertices to animated_vertices, rather than copy
-	//        the data directly.
+    animated_vertices = vertices;
+    // FIXME: blend the vertices to animated_vertices, rather than copy
+    //        the data directly.
 }
 
 
 void Mesh::computeBounds()
 {
-	bounds.min = glm::vec3(std::numeric_limits<float>::max());
-	bounds.max = glm::vec3(-std::numeric_limits<float>::max());
-	for (const auto& vert : vertices) {
-		bounds.min = glm::min(glm::vec3(vert), bounds.min);
-		bounds.max = glm::max(glm::vec3(vert), bounds.max);
-	}
+    bounds.min = glm::vec3(std::numeric_limits<float>::max());
+    bounds.max = glm::vec3(-std::numeric_limits<float>::max());
+    for (const auto& vert : vertices) {
+        bounds.min = glm::min(glm::vec3(vert), bounds.min);
+        bounds.max = glm::max(glm::vec3(vert), bounds.max);
+    }
 }
 
